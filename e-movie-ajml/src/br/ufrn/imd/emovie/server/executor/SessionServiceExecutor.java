95883320677c1a1851e68b4f6f9fa538c5747a3d
danilodamasceno <danilo.g.d@hotmail.com>
2015-12-09 03:59:06 -0300

    Update SessionServiceExecutor.java

--- e-movie-ajml/src/br/ufrn/imd/emovie/server/executor/SessionServiceExecutor.java
+++ e-movie-ajml/src/br/ufrn/imd/emovie/server/executor/SessionServiceExecutor.java
@@ -131,7 +131,8 @@ public class SessionServiceExecutor extends ServiceExecutorTemplate {
 		}
 	}
 
-	/*@ ensures \result != null;
+	/*@ also
+	  @ ensures \result != null;
 	 */
 	@Override
 	public String processPostOther(HttpExchange httpExchange, List<String> urlParams,
